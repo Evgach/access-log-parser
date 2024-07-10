33dcd03f806eabecb7e1daf8e811660251253f28
Volkov Evgeniy <ESVolkov@inno.tech>
2024-07-10 22:00:42 +0300

    В класс Main добавлена логика арифметических операций

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+text.length());
+        System.out.println("Введите первое число");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма введённых чисел равна " + (firstNumber+secondNumber));
+        System.out.println("Разность введённых чисел равна " + (firstNumber-secondNumber));
+        System.out.println("Произведение введённых чисел равно " + (firstNumber*secondNumber));
+        System.out.println("Частное введённых чисел равно " + (double) firstNumber/secondNumber);
+
     }
 }
