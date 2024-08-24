e0f626f6c0cac5f8f936cfb19ede86619c301d79
Volkov Evgeniy <eightshaman@gmail.com>
2024-08-24 15:29:51 +0300

    В методе Main реализована логика: 1. Приложение запускается и читает файл построчно, измеряя длину каждой строки 2. Приложение выводит общее количество строк в файле 3. Приложение выводит длину самой короткой строки 4. Приложение выводит длину самой длинной строки 5. В случае, если в файле встречается строка длиннее 1024 символов, приложение выбрасывает исключение с сообщением "Длина строки №строки больше 1024 символов. Для исключения создан отдельный класс LengthException

---
+++ src/LengthException.java
@@ -0,0 +1,10 @@
+public class LengthException extends Exception {
+
+    public LengthException() {
+        super();
+    }
+
+    public LengthException(String message) {
+        super(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,54 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int counter = 0;
+        int counter = 0; // Счётчик количества файлов
+        int totalNumberOfLinesInTheFile = 0; // Счётчик общего количества строк в файле
+        int shortestLineInTheFile = 1025; // Длина самой короткой строки в файле
+        int longestLineInTheFile = 0; // Длина самой длинной строки в файле
+
         while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
             if (!fileExists || isDirectory)
-                System.out.println("РЈРєР°Р·Р°РЅРЅС‹Р№ С„Р°Р№Р» РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚ РёР»Рё СѓРєР°Р·Р°РЅРЅС‹Р№ РїСѓС‚СЊ СЏРІР»СЏРµС‚СЃСЏ РїСѓС‚С‘Рј Рє РїР°РїРєРµ");
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке");
             else {
                 counter += 1;
-                System.out.println("РџСѓС‚СЊ СѓРєР°Р·Р°РЅ РІРµСЂРЅРѕ");
-                System.out.println("Р­С‚Рѕ С„Р°Р№Р» РЅРѕРјРµСЂ " + counter);
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + counter);
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader = new BufferedReader(fileReader);
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+                        totalNumberOfLinesInTheFile += 1;
+                        if (length > 1024) {
+                            throw new LengthException("Длина строки №" + totalNumberOfLinesInTheFile + " больше 1024 символов");
+                        }
+                        if (length < shortestLineInTheFile) {
+                            shortestLineInTheFile = length;
+                        }
+                        if (length > longestLineInTheFile) {
+                            longestLineInTheFile = length;
+                        }
+
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    break;
+                }
+                System.out.println("Общее количество строк в файле: " + totalNumberOfLinesInTheFile);
+                totalNumberOfLinesInTheFile = 0; // Приводим счётчик к исходному значению
+                System.out.println("Самая короткая строка в файле: " + shortestLineInTheFile);
+                shortestLineInTheFile = 1025; // Приводим счётчик к исходному значению
+                System.out.println("Самая длинная строка в файле: " + longestLineInTheFile);
+                longestLineInTheFile = 0; // Приводим счётчик к исходному значению
             }
         }
     }
